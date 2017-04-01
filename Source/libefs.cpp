470ccf9aa1ee11dd2c4ed04e40e13e7a4c971db8
brandonyeoxg <brandonyeoxg@gmail.com>
2017-04-01 19:53:34 +0800

    Attempt to understand the api and implement logic accordingly

--- Source/libefs.cpp
+++ Source/libefs.cpp
@@ -1,4 +1,6 @@
 #include "libefs.h"
+#define UNASSIGNED_BLOCK 0
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
 // FS Descriptor
 TFileSystemStruct *_fs;
@@ -13,6 +15,15 @@ int _oftCount=0;
 // other functions
 void initFS(const char *fsPartitionName, const char *fsPassword)
 {
+    mountFS(fsPartitionName, fsPassword);
+    _fs = getFSInfo();
+    _oft = new TOpenFile;
+    _oft->inodeBuffer = makeInodeBuffer();
+    _oft->buffer = makeDataBuffer();
+    _oft->blockSize = _fs->blockSize;
+    _oft->writePtr = 0;
+    _oft->readPtr = 0;
+    _oft->filePtr = 0;
 }
 
 // Opens a file in the partition. Depending on mode, a new file may be created
@@ -22,29 +33,112 @@ void initFS(const char *fsPartitionName, const char *fsPassword)
 
 int openFile(const char *filename, unsigned char mode)
 {
-	return -1;
+    _oft->openMode = mode;
+    unsigned int inodeId = findFile(filename);
+    if (inodeId != FS_FILE_NOT_FOUND) { // File exist we just return
+        _oft->inode = inodeId;
+        _oft->writePtr = 0;
+        _oft->readPtr = 0;
+        _oft->filePtr = 0;       
+        loadInode(_oft->inodeBuffer, inodeId); 
+        return inodeId;
+    } 
+    if (mode == MODE_CREATE) {
+        // Create a new file
+        FILE *fp = fopen(filename, "ab+");
+        if (fp == NULL) {
+            return -1;
+        }
+        unsigned int dirIdx = makeDirectoryEntry(filename, 0x0, 0);
+        saveInode(_oft->inodeBuffer, dirIdx);
+        unsigned long freeBlock = findFreeBlock();
+        markBlockBusy(freeBlock);
+        _oft->inodeBuffer[0] = freeBlock;
+        _oft->inode = dirIdx;
+        _oft->writePtr = 0;
+        _oft->readPtr = 0;
+        _oft->filePtr = 0;
+        updateFreeList();
+        updateDirectory();
+        return dirIdx;
+    }
+    return -1;
 }
 
 // Write data to the file. File must be opened in MODE_NORMAL or MODE_CREATE modes. Does nothing
 // if file is opened in MODE_READ_ONLY mode.
 void writeFile(int fp, void *buffer, unsigned int dataSize, unsigned int dataCount)
 {
+    if (_oft->openMode == MODE_READ_ONLY) {
+        return;
+    }
+    _oft->inode = fp;
+    loadInode(_oft->inodeBuffer, fp);
+    if (_oft->openMode != MODE_READ_APPEND) {
+        unsigned int totalData = dataSize * dataCount;
+        unsigned numBlocksReq = (totalData / _fs->blockSize);
+        if (totalData % _fs->blockSize > 0) {
+            numBlocksReq += 1;
+        }
+        int i;
+        for (i = 0; i < numBlockReq; i++) {
+            unsigned long len = fread((char*)_oft->buffer, sizeof(char), _fs->blockSize, fp);
+            if (len < _fs->blockSize0 {
+                continue;
+            }
+            if (_oft->inodeBuffer[i] == UNASSIGNED_BLOCK) { // Inode without block numbers
+                unsigned long freeBlock = findFreeBlock();
+                markBlockBusy(freeBlock);
+                _oft->inodeBuffer[i] = freeBlock;
+                writeBlock(_oft->buffer,freeBlock);
+            } else { // Inode with existing block numbers
+                writeBlock(_oft->buffer, _oft->inodeBuffer[i]);    
+            }
+        }
+    }
+    updateFreeList();
+    updateDirectory();
 }
 
 // Flush the file data to the disk. Writes all data buffers, updates directory,
 // free list and inode for this file.
 void flushFile(int fp)
 {
+    loadInode(_oft->inodeBuffer, fp);
+    unsigned long freeBlock = findFreeBlock();
+    markBlockBusy(freeBlock);
+    writeBlock(_oft->buffer, freeBlock);
+    updateFreeList();
+    updateDirectory();
 }
 
 // Read data from the file.
 void readFile(int fp, void *buffer, unsigned int dataSize, unsigned int dataCount)
 {
+    _oft->inode = fp;
+    loadInode(_oft->inodeBuffer, fp);
+    while (_oft->inodeBuffer[_oft->readPtr] != UNASSIGNED_BLOCK) {
+        readBlock(_oft->buffer, _oft->inodeBuffer[oft->readPtr]);
+        fwrite(buffer, sizeof(char), _fs->blockSize, fp);
+        _oft->readPtr += 1;
+    }
 }
 
 // Delete the file. Read-only flag (bit 2 of the attr field) in directory listing must not be set. 
 // See TDirectory structure.
-void delFile(const char *filename);
+void delFile(const char *filename) {
+    unsigned int inodeId = findFile(filename);
+    if (inodeId == FS_FILE_NOT_FOUND) {
+        return;
+    }
+
+    _oft->inode = inodeId;
+    loadInode(_oft->inodeBuffer, inodeId);
+    unsigned int attr = getAttr(filename);
+    if (CHECK_BIT(attr,2)) {  
+        return;
+    }
+}
 
 // Close a file. Flushes all data buffers, updates inode, directory, etc.
 void closeFile(int fp);
