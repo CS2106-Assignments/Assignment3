14bc8d13be4b22ad09d827d063ca88f694f7b82a
brandonyeoxg <brandonyeoxg@gmail.com>
2017-04-01 22:54:27 +0800

    Start on checkin and checkout, not working at all since libefs is not properly implemented

--- Source/checkin.cpp
+++ Source/checkin.cpp
@@ -2,11 +2,36 @@
 
 int main(int ac, char **av)
 {
-	if(ac != 3)
-	{
-		printf("\nUsage: %s <file to check in> <password>\n\n", av[0]);
-		return -1;
-	}
+    if(ac != 3)
+    {
+        printf("\nUsage: %s <file to check in> <password>\n\n", av[0]);
+        return -1;
+    }
 
-	return 0;
+    FILE *fp = fopen(av[1], "r");
+
+    if (fp == NULL) {
+        printf("\nUnable to open source file %s\n\n", av[1]);
+        exit(-1);
+    }
+
+    // Mount the file system
+    mountFS("part.dsk", av[2]);
+    int status = openFile(av[1], MODE_CREATE);
+
+    if (status == FS_DUPLICATE_FILE) {
+        printf("\nDuplicate files! %s\n\n", av[1]);
+        exit(-1);
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    unsigned int sz = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+    char *buffer;
+    fread( buffer, sizeof(char), sz, fp);
+    writeFile(status, buffer, sizeof(char), sz);
+    fclose(fp);
+    closeFile(status);
+    closeFS();
+    return 0;
 }
--- Source/checkout.cpp
+++ Source/checkout.cpp
@@ -7,6 +7,21 @@ int main(int ac, char **av)
 		printf("\nUsage: %s <file to check out> <password>\n\n", av[0]);
 		return -1;
 	}
-
+    initFS("part.dsk", av[2]);
+    int status = openFile(av[1], MODE_READ_ONLY);
+    
+    if (status == FS_FILE_NOT_FOUND) {
+   		printf("\nFile %s not found!\n\n", av[1]);
+		return -1;
+    }
+    char *buffer;
+    
+    unsigned int len = getFileLength(av[1]);
+    readFile(status, buffer, sizeof(char), len);
+    closeFile(status);
+    FILE *fp = fopen(av[1], "w");
+    fwrite(buffer, sizeof(char), len, fp);
+    fclose(fp);
+    closeFS();
 	return 0;
 }
