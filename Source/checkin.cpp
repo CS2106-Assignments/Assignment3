dbefbda860a64c5f66486e1848ec0d6c83b4aff2
Ryan <ghost_heartbeat@yahoo.com.sg>
2017-04-08 13:30:56 +0800

    Further fixes to checkin.cpp

--- Source/checkin.cpp
+++ Source/checkin.cpp
@@ -9,15 +9,6 @@ int main(int ac, char **av)
 	}
 
 	return 0;
-/*
-	FILE *fp = fopen(av[1], "r");
-
-        if(fp == NULL)
-        {
-                printf("\nUnable to open source file %s\n\n", av[1]);
-                exit(-1);
-        }
-*/
 
 	int fp = openFile(av[1], "r");
 
@@ -27,71 +18,43 @@ int main(int ac, char **av)
 	}
 
 	// Load the file system
-	//mountFS("part.dsk", av[2]);
 	initFS("part.dsk", av[2]);
 
-	// Get the FS metadata
-	//TFileSystemStruct *fs = getFSInfo();
-	TFileSystemStruct *fs = getFSInfo();
+	FILE *pFile = fopen(av[1], "r");
 
-	//char *buffer;
+	// obtain file size:
+	fseek(pFile, 0, SEEK_END);
+	long lSize = ftell(pFile);
+	rewind(pFile);
+
 	char *buffer;
 
-	// Allocate the buffer for reading
-	//buffer = makeDataBuffer();
-	buffer = makeDataBuffer();
+	// allocate memory to contain the whole file:
+	buffer = (char*)malloc(sizeof(char)*lSize);
+
+	// copy the file into the buffer:
+	fread(buffer, 1, lSize, pFile);
+
+	/* the whole file is now loaded in the memory buffer. */
+	
+	buffer = (char*)malloc(sizeof(char)*lSize);
+
+	// copy the file into the buffer:
+	//fread(buffer, sizeof(char), lSize, pFile);
 
 	// Read the file
-	//unsigned long len = fread(buffer, sizeof(char), fs->blockSize, fp);
-	int len = readFile(fp, buffer, sizeof(char), fs->blockSize);
+	int len = readFile(fp, buffer, sizeof(char), lSize);
 
-	// Write the directory entry
-	//unsigned int dirNdx = makeDirectoryEntry(av[1], 0x0, len);
-	unsigned int dirNdx = makeDirectoryEntry(av[1], 0x0, len);
-
-	// Find a free block
-	//unsigned long freeBlock = findFreeBlock();
-	unsigned long freeBlock = findFreeBlock();
-
-	// Mark the free block now as busy
-	//markBlockBusy(freeBlock);
-	markBlockBusy(freeBlock);
-
-	// Create the inode buffer
-	//unsigned long *inode = makeInodeBuffer();
-	unsigned long *inode = makeInodeBuffer();
-
-	// Load the Inode
-	//loadInode(inode, dirNdx);
-	loadInode(inode, dirNdx);
-
-	// Set the first entry of the inode to the free block
-	//inode[0]=freeBlock;
-	inode[0]=freeBlock;
-
-	// Write the data to the block
-	//writeBlock(buffer, freeBlock);
-	writeBlock(buffer, freeBlock);
-
-	// Write the inode
-	//saveInode(inode, dirNdx);
-	saveInode(inode, dirNdx);
-
-	// Write the free list
-	//updateFreeList();
-	updateFreeList();
-
-	// Write the diretory
-	//updateDirectory();
-	updateDirectory();
+	// Write to file
+	writeFile(fp, buffer, sizeof(char), len);
 
 	// Unmount
 	//unmountFS();
 	closeFS();
 
-	// Free data and inode buffer
+	// Free buffer
 	free(buffer);
-	free(inode);
+	//free(inode);
 	return 0;
 
 }
